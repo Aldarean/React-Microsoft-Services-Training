cad7e6cf12546691dab09d397f492cc2822bb7c1
Rodney Wormsbecher <rodney22@live.nl>
2019-06-04 13:40:19 +0200

    refresh token added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from 'react';
-
+import axios from 'axios';
+import { acquireToken, getGraphToken } from './adalConfig';
 import './App.css';
 
-function App() {
-	return <div className="App">test</div>;
+class App extends React.Component {
+	state = {
+		userData: ''
+	};
+
+	async componentDidMount() {
+		await acquireToken();
+		let graphToken = await getGraphToken();
+
+		try {
+			let response = await axios({
+				url: 'https://graph.microsoft.com/v1.0/me',
+				method: 'GET',
+				headers: { Authorization: `Bearer ${graphToken}` }
+			});
+
+			if (response.status != 200 && response.status != 204) {
+				return 'error';
+			}
+
+			this.setState({ userData: response.data });
+		} catch (error) {
+			return 'error';
+		}
+	}
+
+	tokenRefresher = () => {
+		acquireToken();
+	};
+
+	render() {
+		let userName = '';
+
+		setInterval(this.tokenRefresher, 150000);
+
+		if (this.state.userData != '') {
+			userName = this.state.userData.givenName;
+		}
+
+		return <div className="App">Welcome, {userName}</div>;
+	}
 }
 
 export default App;
